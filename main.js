83dec621ac9e4c5bc6f57bd127b06086279a0e7a
Anjamanuellah <manuellahanja@gmail.com>
2022-08-17 07:38:28 +0200

    js day2

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function division(a, b) {
     return a / b;
 }  OU
 */
-
+/*
 var addition = (a, b) => {
     return a + b;
 }
@@ -46,9 +46,9 @@ var division = (a, b) => {
     return a / b;
 }
 
-//condition:
+condition:
 
-/*if raha,
+if raha,
 while rehefa,
 (switch, case)raha toa ka
   */
@@ -65,7 +65,7 @@ else {
     console.log("false");
 }
 */
-
+/*
 function calculatrice(a, b, signe){
     if (signe === "+"){
         return a + b;
@@ -101,8 +101,10 @@ function calculatrice(a, b, signe){
         return a - b;
     }
 }
+/*
 console.log(addition(10,10))
 console.log(soustraction(10,10))
+
 //console.log(division(10, 2));
 //console.log(multiplication(4 , 4))
 //console.log(soustraction(4, 2));
@@ -110,10 +112,72 @@ console.log(soustraction(10,10))
 //console.log(addition(2, 2));
 //console.log(preparing());
 //console.log(vitesse);
-//console.log(color, name);
+console.log(color, name);
 
-/*compilé E var
-fonc 
-cond
+compilé E var
+            fonc 
+            cond
+*/
+____________________________________________________________________________
+/*javascript_d2
+branche creation & basculement & mise a jour & suppretion branche 
+
+git = recherche 
+
+ tableau ARRAY
+tableau zvtr afaana mstocker zavatra 
+stucturé kokoa noho ny var
+
+ligne = asina ny valeur / misy col /
+nb: manisa manomboka 0 (consideration 0 comme nb reel )
+rehefa tab fa msi langage manisa depart 1
+nb ny chaine de caractere tsy voteru "" fa afaka ''
 */
 
+// decl tab 
+
+var alphabet = ["a", "b", "c"];
+for (let pointer = 0; pointer < alphabet.length; pointer++) {
+    console.log(pointer);
+    console.log(alphabet[pointer]);
+} 
+
+
+var alphabet = ["a" , "b" , "c"];
+for (let pointer = alphabet.length-1; pointer >= 0; pointer--) {                        //.length = taille tab
+    console.log(pointer);
+    console.log(alphabet[pointer]);
+}
+/*
+++ = pointer+1
+pointeur(un var)= un cle :initiateur ny tab// alefa mitety ny tab
+ acceder un valeur => bouclena ilay tab
+ tab= tsy multidim am js 
+ tab am js dia obj 
+ tab uni dim
+ tab 2 dim
+ tab multidim tab dans tab 
+*/
+
+var alphabet = ["a" , "b" , "c"];
+//for (let pointer = alphabet.pop-1; pointer >= 0; pointer--) {                        
+ //   console.log(pointer);
+  //  console.log(alphabet[pointer]);
+    console.log(alphabet.pop());                     // manala ny ao arina
+    console.log(alphabet)
+
+    var alphabet =  ["a" , "b" , "c"];
+    alphabet.shift();                   //manala ny ao aloha 
+    console.log(alphabet);
+    
+  
+var alphabet = ["a" , "b" , "c"];
+//console.log(alphabet.join("/"));  //tab => chaine de caract + jointure 
+
+//alphabet.push("Anja");         //mampiditra valeur any arina 
+//console.log(alphabet);
+
+console.table(alphabet);         //afficher tab
+
+//alphabet.includes("b");
+console.log(alphabet.includes("b"));  //misy b ve ao? true / false
