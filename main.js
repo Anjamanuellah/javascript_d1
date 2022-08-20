905a89211545d723e7c6d271b000ed7323f2bd8c
Anjamanuellah <manuellahanja@gmail.com>
2022-08-20 11:51:41 +0300

    lamina

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
+//declaration en js:
+
 var color = "red";
 let name ="anja";
 const vitesse ="10km/h";
 
+/*var color = "bleu" var sy let afaka redeclarena: mutable 
+const tsy afaka redeclarena :immutable 
+nb: un langage compilé = var  fonc   cond 
+ decl fonction = action (return =finalité de la fonction)
+ ao am fonction misy argument 
 
+ex declaration fonction
+ex1:function preparing() {
+   console.log("misakafo");
+   return "go" ; 
+}
+*/
 
-//var color = "bleu"  afaka redeclarena mutable sy let 
-// const tsy afaka redeclarena immutable 
-
-// decl fonction = action (return =finalité an fonction)
-// ao am fonction misy argument 
-//function preparing() {
-   // console.log("misakafo");
-   //return "go" ; 
-
-
-/*
 function addition(a, b) {
     return a + b;
 }
@@ -29,9 +31,11 @@ function multiplication(a, b) {
 
 function division(a, b) {
     return a / b;
-}  OU
-*/
-/*
+} 
+
+//OU
+//ex2:
+
 var addition = (a, b) => {
     return a + b;
 }
@@ -46,26 +50,27 @@ var division = (a, b) => {
     return a / b;
 }
 
-condition:
 
-if raha,
-while rehefa,
-(switch, case)raha toa ka
-  */
-
-  //boucle infinie==> while
-
-  //=== comparer type et valeur
-  //==comparer valeur
+//condition:
 /*
+if =raha,
+while =rehefa,
+(switch, case)=raha toa ka
+
+  boucle infinie==> while
+
+  === comparer type et valeur
+   ==comparer valeur
+*/
+
 if ("bleu" === "red") {
     console.log("true");
 }
 else {
     console.log("false");
 }
-*/
-/*
+
+
 function calculatrice(a, b, signe){
     if (signe === "+"){
         return a + b;
@@ -85,6 +90,7 @@ function calculatrice(a, b, signe){
 }
 
 // do_while
+
 function calculatrice(a, b, signe){
 do {
     a / b;
@@ -101,10 +107,9 @@ function calculatrice(a, b, signe){
         return a - b;
     }
 }
-/*
-console.log(addition(10,10))
-console.log(soustraction(10,10))
 
+//console.log(addition(10,10))
+//console.log(soustraction(10,10))
 //console.log(division(10, 2));
 //console.log(multiplication(4 , 4))
 //console.log(soustraction(4, 2));
@@ -112,15 +117,10 @@ console.log(soustraction(10,10))
 //console.log(addition(2, 2));
 //console.log(preparing());
 //console.log(vitesse);
-console.log(color, name);
-
-compilé E var
-            fonc 
-            cond
-*/
-____________________________________________________________________________
+//console.log(color, name);     
+//____________________________________________________________________________
 /*javascript_d2
-branche creation & basculement & mise a jour & suppression branche 
+recherche sur :branche creation & basculement & mis a jour & suppression branche 
 creatin d' une branche:1. git branch <nom de la branche>
                          git branch (pour afficher la liste de branche)
                     2.git checkout -b <branch name>
@@ -129,21 +129,27 @@ creatin d' une branche:1. git branch <nom de la branche>
 basculement : git checkout (pour afficher l'ancien commit) 
                             (permet de basculer entre les branches crée au moyen de git branch)                      
 mise a jour: merge 
-              rebase
+            rebase
 suppresion de la branche: git checkout
                           git branch -d<nom de la branche>
-git = recherche 
+ pour comprendre le git->recherche 
 
  tableau ARRAY_______
-tableau zvtr afaana mstocker zavatra 
-stucturé kokoa noho ny var
-
+def: tableau zvtr afaana mstocker zavatra 
+     stucturé kokoa noho ny var
 ligne = asina ny valeur / misy col /
-nb: manisa manomboka 0 (consideration 0 comme nb reel )
-rehefa tab fa msi langage manisa depart 1
-nb ny chaine de caractere tsy voteru "" fa afaka ''
+nb: -manisa manomboka 0 (consideration 0 comme nb reel )
+    -rehefa tab fa msi langage manisa depart 1
+     -ny chaine de caractere tsy voteru "" fa afaka ''
+++ = pointer+1
+pointeur(un var)= un cle :initiateur ny tab= alefa mitety ny tab
+ acceder un valeur => bouclena ilay tab
+ tab= tsy multidim @ js 
+ tab am js dia obj 
+ tab uni-dim
+ tab 2-dim
+ tab multidim: tab dans tab 
 */
-
 // decl tab 
 
 var alphabet = ["a", "b", "c"];
@@ -158,39 +164,33 @@ for (let pointer = alphabet.length-1; pointer >= 0; pointer--) {                
     console.log(pointer);
     console.log(alphabet[pointer]);
 }
-/*
-++ = pointer+1
-pointeur(un var)= un cle :initiateur ny tab// alefa mitety ny tab
- acceder un valeur => bouclena ilay tab
- tab= tsy multidim am js 
- tab am js dia obj 
- tab uni dim
- tab 2 dim
- tab multidim tab dans tab 
-*/
+
 
 var alphabet = ["a" , "b" , "c"];
-//for (let pointer = alphabet.pop-1; pointer >= 0; pointer--) {                        
- //   console.log(pointer);
-  //  console.log(alphabet[pointer]);
-    console.log(alphabet.pop());                     // manala ny ao arina
+    alphabet.pop();                      //.pop= manala ny ao arina
     console.log(alphabet)
 
+
     var alphabet =  ["a" , "b" , "c"];
-    alphabet.shift();                   //manala ny ao aloha 
+    alphabet.shift();                     //.shift=manala ny ao aloha 
     console.log(alphabet);
-    
-  
+
+
 var alphabet = ["a" , "b" , "c"];
-//console.log(alphabet.join("/"));  //tab => chaine de caract + jointure 
+console.log(alphabet.join("/"));     //.join=jointure du tab => chaine de caract + jointure 
 
-//alphabet.push("Anja");         //mampiditra valeur any arina 
-//console.log(alphabet);
 
+var alphabet = ["a" , "b" , "c"];
+alphabet.push("Anja");             //mampiditra valeur any arina 
+console.log(alphabet);
+
+
+var alphabet = ["a" , "b" , "c"];
 console.table(alphabet);         //afficher tab
 
-//alphabet.includes("b");
-console.log(alphabet.includes("b"));  //misy "b" ve ao? true / false
 
+var alphabet = ["a" , "b" , "c"];
+alphabet.includes("b");
+console.log(alphabet.includes("b"));  //misy "b" ve ao? (true / false)
 
 
